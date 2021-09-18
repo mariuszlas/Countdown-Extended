7f47181a2187187958d6dd63942b5b29fbe74b12
mariuszlas <mariusz.las@outlook.com>
2021-09-18 17:13:58 +0100

    Added database uri

--- api/dbConfig/init.js
+++ api/dbConfig/init.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+
 const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT
-});
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  });
+
+// const pool = new Pool({
+//     user: process.env.PGUSER,
+//     host: process.env.PGHOST,
+//     database: process.env.PGDATABASE,
+//     password: process.env.PGPASSWORD,
+//     port: process.env.PGPORT
+// });
 
 module.exports = pool;
