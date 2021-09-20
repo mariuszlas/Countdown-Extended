748a1692fc11a53418687b849b67b24f691b7b7d
Gorazd Sumkovski <gorazd.sumkovski@gmail.com>
2021-09-20 16:31:32 +0100

    Added countdown circle timer

--- client/src/pages/QuizPage/index.js
+++ client/src/pages/QuizPage/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
 import { cleanString } from "../../actions";
 
@@ -7,6 +8,7 @@ export const QuizPage = () => {
 
     const questionsArr = useSelector(state => state.questions);
     
+    const [key, setKey] = useState(0);
     const [n, setN] = useState(0);
 
     const dispatch = useDispatch();
@@ -15,10 +17,11 @@ export const QuizPage = () => {
 
     const c_answer = question.correct_answer;
     const i_answers = question.incorrect_answers.slice(0, 3); // shouldn't need the .slice but first question is being weird
-
+    
     function submitAnswer(e) {
         const submission = e.target.value;
         submission === c_answer ? dispatch({type: 'UPDATE_SCORE', payload: 1}) : console.log('oops, wrong answer');
+        setKey(x => ++x);
         setN(x => ++x);
     }
 
@@ -33,12 +36,30 @@ export const QuizPage = () => {
 
     const answers = randQuestionDist();
 
+
     if (n <= 9) {
 
         return (
         <>
         
         <h1>{cleanString(question.question)}</h1>
+
+        <CountdownCircleTimer
+            onComplete={() => {
+                setKey(x => ++x);
+                setN(x => ++x);
+            }}
+            key={key}
+            isPlaying
+            duration={10}
+            colors={[
+            ['#004777', 0.33],
+            ['#F7B801', 0.33],
+            ['#A30000', 0.33],
+            ]}
+        >
+            {({ remainingTime }) => remainingTime}
+        </CountdownCircleTimer>
 
         {answers.map((ans, index) => (
             <button onClick={submitAnswer} value={ans} key={index}>{cleanString(ans)}</button>
