da9df8ca1cf4caf11f2b007cbd55a5b17248367c
mariuszlas <mariusz.las@outlook.com>
2021-09-20 08:36:38 +0100

    moved triva api request to client side

--- api/dbConfig/seedDev.js
+++
@@ -1,5 +0,0 @@
-const db = require('./init.js');
-const fs = require('fs');
-
-const seeds = fs.readFileSync(__dirname + '/dev_seeds.sql').toString();
-db.query(seeds, () => console.log('Development database initialised'));
