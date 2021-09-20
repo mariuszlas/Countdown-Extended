67d91060471a1d2e075c5ecfeb50d52204c92641
jasminappleby <jasminapplebyholdham@hotmail.com>
2021-09-20 10:19:46 +0100

    added basic structure for timer component and for app.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+//import pages and layouts here
+
+//import app css here
+
+function App(){
+
+    return(
+      <>
+        {/* layouts and pages go here */}
+      </>
+    );
+  
+  };
+  
+  export default App;
+  
