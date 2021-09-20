d11a9b8f3de42dd5e010b3555834b0d38977f4e6
jasminappleby <jasminapplebyholdham@hotmail.com>
2021-09-20 17:38:08 +0100

    added header and footer back

--- client/src/App.js
+++ client/src/App.js
@@ -8,29 +8,31 @@ import './index.css';
 function App() {
     return (
         <>
-            <Switch>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route path="/game-setup">
-                    <GameSetup />
-                </Route>
-                <Route path="/join-room">
-                    <JoinRoom />
-                </Route>
-                <Route path="/waiting-room">
-                    <WaitingRoom />
-                </Route>
-                <Route path="/game-results">
-                    <GameResults />
-                </Route>
-                <Route path="/all-results">
-                    <AllResults />
-                </Route>
-                <Route path="/quiz-page">
-                    <QuizPage />
-                </Route>
-            </Switch>
+            <Header />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/game-setup">
+                        <GameSetup />
+                    </Route>
+                    <Route path="/join-room">
+                        <JoinRoom />
+                    </Route>
+                    <Route path="/waiting-room">
+                        <WaitingRoom />
+                    </Route>
+                    <Route path="/game-results">
+                        <GameResults />
+                    </Route>
+                    <Route path="/all-results">
+                        <AllResults />
+                    </Route>
+                    <Route path="/quiz-page">
+                        <QuizPage />
+                    </Route>
+                </Switch>
+            <Footer />
         </>
     );
 }
