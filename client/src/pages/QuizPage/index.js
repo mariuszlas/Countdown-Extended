949885f25234118d74d8de539c49ebaae14fc7bc
Gorazd Sumkovski <gorazd.sumkovski@gmail.com>
2021-09-20 16:16:51 +0100

    Answers are now rendered as 4 buttons in random order

--- client/src/pages/QuizPage/index.js
+++ client/src/pages/QuizPage/index.js
@@ -12,13 +12,26 @@ export const QuizPage = () => {
     const dispatch = useDispatch();
 
     const question = questionsArr[Math.min(n, 9)];
+
     const c_answer = question.correct_answer;
+    const i_answers = question.incorrect_answers.slice(0, 3); // shouldn't need the .slice but first question is being weird
 
     function submitAnswer(e) {
         const submission = e.target.value;
         submission === c_answer ? dispatch({type: 'UPDATE_SCORE', payload: 1}) : console.log('oops, wrong answer');
         setN(x => ++x);
     }
+
+    function randQuestionDist() {
+        const randIdx = Math.floor(4 * Math.random());
+        let arr = i_answers;
+
+        arr.splice(randIdx, 0, c_answer);
+
+        return arr
+    }
+
+    const answers = randQuestionDist();
 
     if (n <= 9) {
 
@@ -27,15 +40,9 @@ export const QuizPage = () => {
         
         <h1>{cleanString(question.question)}</h1>
 
-        <button onClick={submitAnswer} value={c_answer}>{c_answer}</button>
-        <button onClick={submitAnswer} value={question.incorrect_answers[2]}>{question.incorrect_answers[2]}</button>
-
-        {/* <ul>
-            <li>{questions[0].correct_answer}</li>
-            <li>{questions[0].incorrect_answers[0]}</li>
-            <li>{questions[0].incorrect_answers[1]}</li>
-            <li>{questions[0].incorrect_answers[2]}</li>
-        </ul> */}
+        {answers.map((ans, index) => (
+            <button onClick={submitAnswer} value={ans} key={index}>{cleanString(ans)}</button>
+        ))}
 
         </>
         )
