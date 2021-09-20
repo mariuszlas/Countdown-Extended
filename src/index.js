2e8d83f3fa9981b708b7c0fcf80167fb4d1d837a
jasminappleby <jasminapplebyholdham@hotmail.com>
2021-09-20 10:25:18 +0100

    added basic file structure for public and for the src/index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//basic root, will be altered later
+
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
