f29721bea9e705c7dc5b462385943c5bd6eb8260
mariuszlas <mariusz.las@outlook.com>
2021-09-20 11:57:52 +0100

    fix dev dependecies in package.json

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from './redux/store';
 import App from './App';
 
