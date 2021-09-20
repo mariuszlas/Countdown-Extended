5ac915c6ee4571fd4dc913b42a4c65cae01329b8
Tobi Kuyoro <tobikuyo@gmail.com>
2021-09-20 16:53:59 +0100

    Fix reducer and delete ScoreList file

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -6,4 +6,3 @@ export { default as Home } from './Home';
 export { default as JoinRoom } from './JoinRoom';
 export { default as WaitingRoom } from './WaitingRoom';
 export { default as QuizPage } from './QuizPage';
-export { default as ScoreList } from './ScoreList';
