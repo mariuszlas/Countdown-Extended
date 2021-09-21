132f8c455b2b738bebe1f301460850ed8cfd2f47
Tobi Kuyoro <tobikuyo@gmail.com>
2021-09-21 15:03:01 +0100

    Create table to track usernames

--- db/1_setup.sql
+++ db/1_setup.sql
@@ -1,7 +1,10 @@
-DROP TABLE IF EXISTS scores;
-
-CREATE TABLE scores (
+CREATE TABLE IF NOT EXISTS scores (
     id serial PRIMARY KEY,
     username VARCHAR(500) NOT NULL,
     score FLOAT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS usernames (
+    id serial PRIMARY KEY,
+    name VARCHAR(500) NOT NULL UNIQUE
 );
