689e58d0b8eed39dcfb66da4aaf4ea2cd8d6f3b8
Gorazd Sumkovski <gorazd.sumkovski@gmail.com>
2021-09-22 15:24:49 +0100

    Added test for failure of addUsername method

--- api/test/unit/models/Username.spec.js
+++ api/test/unit/models/Username.spec.js
@@ -13,4 +13,15 @@ describe('Username model', () => {
         jest.spyOn(db, 'query').mockResolvedValue({ rows: [testValue] });
         expect(result).toBeInstanceOf(Username);
     });
+
+    test('returns error notifying failure of creation on unsuccessful db query', async () => {
+        const testValue = { name: 'username' };
+
+        try {
+            jest.spyOn(db, 'query').mockRejectedValueOnce('Error occurred during creation');
+            await Username.addUsername(testValue);
+        } catch (err) {
+            expect(err).toEqual('Error occurred during creation');
+        }
+    })
 });
