94cb9e2dc11e179ab5e574c131d1c734c427b4d0
Gorazd Sumkovski <gorazd.sumkovski@gmail.com>
2021-09-22 15:30:45 +0100

    Added tests for the failures of the create and all methods

--- api/test/unit/models/Score.spec.js
+++ api/test/unit/models/Score.spec.js
@@ -11,19 +11,39 @@ describe('Score', () => {
     afterAll(() => jest.resetAllMocks());
 
     describe('all', () => {
-        it('resolves with a list of objects on sucessful databse query', async () => {
+        it('resolves with a list of objects on successful databse query', async () => {
             jest.spyOn(db, 'query').mockResolvedValueOnce({ rows: [{}, {}, {}, {}] });
             const all = await Score.all;
             expect(all).toHaveLength(4);
         });
+
+        test('returns error notifying failure of retrieval on unsuccessful db query', async () => {
+            try {
+                jest.spyOn(db, 'query').mockRejectedValueOnce();
+                await Score.all;
+            } catch (err) {
+                expect(err).toEqual('Error in retrieving scores from database');
+            }
+        })
     });
 
     describe('create', () => {
-        it('resolves with the newly created score on sucessful databse query', async () => {
+        it('resolves with the newly created score on successful databse query', async () => {
             const testObj = { username: 'test_username', score: 200 };
             jest.spyOn(db, 'query').mockResolvedValueOnce({ rows: [testObj] });
             const result = await Score.create(testObj);
             expect(result).toBeInstanceOf(Score);
         });
+
+        test('returns error notifying failure of creation on unsuccessful db query', async () => {
+            const testObj = { username: 'test_username', score: 200 };
+
+            try {
+                jest.spyOn(db, 'query').mockRejectedValueOnce();
+                await Score.create(testObj);
+            } catch (err) {
+                expect(err).toEqual('Could not add the score to database');
+            }
+        })
     });
 })
