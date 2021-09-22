ac4112148e7049740673eefe1ea46564e3b48ceb
Tobi Kuyoro <tobikuyo@gmail.com>
2021-09-22 10:20:14 +0100

    Create results state and an action to update it

--- client/src/redux/constants.js
+++ client/src/redux/constants.js
@@ -5,4 +5,5 @@ export const ADD_QUESTIONS = 'ADD_QUESTIONS';
 export const CURRENT_PLAYER = 'CURRENT_PLAYER';
 export const SET_ERROR = 'SET_ERROR';
 export const UPDATE_SCORE = 'UPDATE_SCORE';
-export const RESET_SCORE = 'RESET_SCORE'
+export const RESET_SCORE = 'RESET_SCORE';
+export const UPDATE_RESULTS = 'UPDATE_RESULTS';
