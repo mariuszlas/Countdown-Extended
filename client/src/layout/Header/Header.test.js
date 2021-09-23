b017c453d1968c99be9b76f42de99a1c98591237
Gorazd Sumkovski <gorazd.sumkovski@gmail.com>
2021-09-23 19:35:21 +0100

    Added test for clicking the logo

--- client/src/layout/Header/Header.test.js
+++ client/src/layout/Header/Header.test.js
@@ -1,11 +1,25 @@
 import { default as Header } from '.';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+
+const mockHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    useHistory: () => ({
+        push: mockHistoryPush
+    }),
+}))
 
 describe('Header', () => {
 
     it('renders header', () => {
         renderWithReduxAndRouter(<Header />);
         const img = screen.getByRole('img');
+        
         expect(img).toBeInTheDocument();
+
+        userEvent.click(img);
+
+        expect(mockHistoryPush).toHaveBeenCalledWith('/');
     });
 })
